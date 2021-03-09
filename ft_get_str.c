01f1a2fe55ecd9e0e3b08356e045e4b0daa0822d
Bouterfass <cefyou28@gmail.com>
2021-03-09 12:06:01 +0100

    clean files

--- ft_get_str.c
+++
@@ -1,9 +0,0 @@
-#include "libftprintf.h"
-
-
-int ft_get_str(ft_specs *spe, const char *s, int i)
-{
-    spe->str = (char *)malloc(sizeof(char) * (ft_speclen(s, i) - i));
-    ft_memcpy(spe->str, ft_substr(s, i, ft_speclen(s, i) - (i - 1)), ft_speclen(s, i) - (i - 1));
-    return (ft_speclen(s, i));
-}
